945ff714ab184a396a3ac062fcfd5a1ff96f00aa
Divya Prem <dkothand@andrew.cmu.edu>
2021-04-29 23:30:58 -0400

    Set up initial repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {/* style the div below so "LEFT" is on the left side and "RIGHT" is on the right side */}
+      <StyledContent>LEFT RIGHT</StyledContent>
+
+      {/* create a button & then show an alert when user clicks the button */}
     </div>
   );
 }
 
 export default App;
+
+const StyledContent = styled.div`
+  border: 2px solid blue;
+  width: 50%;
+  margin: auto;
+`;
